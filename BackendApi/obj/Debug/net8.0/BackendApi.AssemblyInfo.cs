31086c63a4d19c040c9e1d87d4f9d14348495ea9
nguyenlevytinh <hungnguyen170801@gmail.com>
2025-11-21 10:14:38 +0700

    Add Dockerfile + production config

--- BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net8.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+499841dbe3a44b08c0884c6454f255df83cc6499")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ea8c3ecd328e42bbe0fca78153e1d021f05f63")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
